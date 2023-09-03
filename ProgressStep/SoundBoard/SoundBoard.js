e6cb50e57a708127056e8309f254a780bc606b27
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-09-03 22:46:53 +0530

    soundboard

---
+++ ProgressStep/SoundBoard/SoundBoard.js
@@ -0,0 +1,20 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText = sound
+    btn.addEventListener('click', () => {
+        stopSounds()
+        document.getElementById(sound).play()
+    })
+    const Button = document.getElementById('button')
+    Button.appendChild(btn)
+})
+
+function stopSounds() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+        song.pause()
+        song.currentTime = 0;
+    })
+}
